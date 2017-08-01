38120fa884815427e789af19d4b0a9715a2b3f19
hexson <1477546184@qq.com>
2017-08-01 21:18:55 +0800

    update components and less style

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,7 +2,7 @@
   'use strict';
 
   angular.module('app')
-  .controller('footer', ['$scope', function(vm){
+  .controller('footer', ['$scope', '$rootScope', '$state', function(vm, $rootScope, $state){
     var height = vm.indexWidth / 720;
     vm.indexFooterStyle = 'height:' + (height * 144) + 'px';
   }]);
